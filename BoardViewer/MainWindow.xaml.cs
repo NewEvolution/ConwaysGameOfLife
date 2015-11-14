8f9b561192184db3b9d76464f348216b670405bf
Ryan Tanay <ryantanay@newevolution.org>
2015-11-13 19:51:51 -0600

    Changed all preset patterns to be mostly centered on a 71x71 grid.

--- BoardViewer/MainWindow.xaml.cs
+++ BoardViewer/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ namespace BoardViewer
 
         public MainWindow()
         {
-            currentBoard = new GameOfLife(72, 71, "1357/1357", "71 pentadecathlon");
+            currentBoard = new GameOfLife(72, 71, "23/3", "pentadecathlon");
             dispatcherTimer = new DispatcherTimer();
 
             InitializeComponent();
